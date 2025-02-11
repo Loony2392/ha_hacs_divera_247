5cf7efa3d4a50c53902f298c5ccb608aa2bd26fc
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-11 23:16:57 +0100

    Update prep_release.sh
    Fix: Names

--- scripts/prep_release.sh
+++ scripts/prep_release.sh
@@ -3,5 +3,5 @@
 VERSION=$1
 
 cd custom_components/divera247
-sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${VERSION}\"/g" yourfile.json
-zip divera.zip -r ./
+sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${VERSION}\"/g" manifest.json
+zip divera247.zip -r ./
