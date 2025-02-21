ef7b324257dce94803f89efeca2396710eb5c518
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-21 20:18:18 +0100

    Entferne veraltete Dokumentationsdateien und -skripte

--- docs/make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set SPHINXBUILD=sphinx-build
-set SOURCEDIR=.
-set BUILDDIR=_build
-
-%SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIR%/html
-echo.
-echo Build fertig. Die HTML-Dateien befinden sich in %BUILDDIR%/html.
-pause
