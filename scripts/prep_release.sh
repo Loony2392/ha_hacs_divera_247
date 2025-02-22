fb687bab9f5d7cb06b9e4e4def67374d30bb840b
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-22 23:33:23 +0100

    chore: update prep_release.sh to change directory and update version in manifest.json

--- scripts/prep_release.sh
+++ scripts/prep_release.sh
@@ -2,14 +2,14 @@
 
 VERSION=$1
 
-# Ändere das Verzeichnis zu custom_components/divera247
+# Change directory to custom_components/divera247
 cd custom_components/divera247
 
-# Aktualisiere die Version in der manifest.json Datei
+# Update the version in the manifest.json file
 sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${VERSION}\"/g" manifest.json
 
-# Gehe zurück zum Root-Verzeichnis des Repositories
+# Go back to the root directory of the repository
 cd ../..
 
-# Erstelle eine ZIP-Datei, die nur den Ordner divera247 enthält
+# Create a ZIP file that contains only the divera247 folder
 zip -r divera247.zip custom_components/divera247
