dbac49015c9944768396d03c6e60ab6836875f1b
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-22 19:14:32 +0100

    refactor: move prep_release.sh to scripts directory and update versioning logic

--- scripts/prep_release.sh
+++ scripts/prep_release.sh
@@ -2,6 +2,14 @@
 
 VERSION=$1
 
+# Ändere das Verzeichnis zu custom_components/divera247
 cd custom_components/divera247
+
+# Aktualisiere die Version in der manifest.json Datei
 sed -i "s/\"version\": \"[0-9]\+\.[0-9]\+\.[0-9]\+\"/\"version\": \"${VERSION}\"/g" manifest.json
-zip divera247.zip -r ./
+
+# Gehe zurück zum Root-Verzeichnis des Repositories
+cd ../..
+
+# Erstelle eine ZIP-Datei, die nur den Ordner divera247 enthält
+zip -r divera247.zip custom_components/divera247
