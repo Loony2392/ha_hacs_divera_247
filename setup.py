e37486a0c67e51e1143bcb8f12234ae3382397ad
Loony <77867402+Loony2392@users.noreply.github.com>
2025-02-23 21:18:26 +0100

    chore: format setup.py for consistency and clarity

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='divera247',
-    version='1.0.0',
+    name="divera247",
+    version="1.0.0",
     packages=find_packages(),
-    install_requires=[
-
-    ],
+    install_requires=[],
     entry_points={
-        'console_scripts': [
-
-        ],
+        "console_scripts": [],
     },
 )
