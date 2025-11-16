b8f450b1b8378c600a139f2d34461dd97cca7eb2
Loony <77867402+Loony2392@users.noreply.github.com>
2025-11-16 21:43:12 +0100

    chore(git): ignore .haconfig and remove local HA artifacts

--- .haconfig/deps/lib/python/site-packages/matter_server/common/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants that are shared between server and client."""
-
-# schema version is used to determine compatibility between server and client
-# bump schema if we add new features and/or make other (breaking) changes
-SCHEMA_VERSION = 8
